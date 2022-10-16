796d04461f9ab1acedd9beda28617a66a69671d5
erikgreasy <erik.masny@gmail.com>
2022-10-17 00:57:58 +0200

    feat: add pinia

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,8 @@ import 'flowbite';
 
 import { createApp } from 'vue';
 import { createWebHistory, createRouter } from "vue-router";
+import { createPinia } from "pinia";
+
 import routes from './routes.js';
 import App from './App.vue';
 
@@ -15,5 +17,7 @@ const router = createRouter({
 const app = createApp(App)
 
 app.use(router)
+
+app.use(createPinia());
 
 app.mount('#app')
--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,13 +1,20 @@
 <template>
     <div>
-        Home
+        Hello {{ store.user?.name }}
     </div>
 </template>
 
-<script>
-export default {
+<script setup>
+import { onMounted } from '@vue/runtime-core';
+import axios from 'axios';
+import { useAuthStore } from './../stores/auth';
 
-}
+const store = useAuthStore()
+
+onMounted(async () => {
+    const res = await axios.get('/api/user')
+    store.user = res.data
+})
 </script>
 
 <style>

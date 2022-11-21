40c69bcd495dcf4ea78cedea71078f34b9abbb07
Erik Masny <erik.masny@gmail.com>
2022-11-21 21:42:11 +0100

    feat: add transitions to route change

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,11 @@
 <template>
-    <component :is="this.$route.meta.layout || Public">
-        <router-view />
+    <component :is="$route.meta.layout || Public">
+        <router-view v-slot="{Component}">
+            <transition name="fade" mode="out-in">
+                <component :is="Component" :key="$route.path"></component>
+            </transition>
+        </router-view>
+
     </component>
 </template>
 
@@ -25,3 +30,17 @@ onBeforeMount(async () => {
     }
 })
 </script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: .3s;
+  transition-property: opacity;
+  transition-timing-function: ease-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0
+}
+</style>

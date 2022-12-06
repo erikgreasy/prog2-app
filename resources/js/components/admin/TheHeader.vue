62961a45a9e2a4b8e4f4a15ab699d281fcb63b22
Erik Masny <erik.masny@gmail.com>
2022-12-06 09:11:49 +0100

    refactor: split admin layout into partials

---
+++ resources/js/components/admin/TheHeader.vue
@@ -0,0 +1,9 @@
+<template>
+    <header class="border-b border-gray-200 h-12 fixed top-0 w-full z-10 px-5 flex items-center bg-white">
+        <AppLogo />
+    </header>
+</template>
+
+<script setup>
+import AppLogo from '@/components/AppLogo.vue';
+</script>
---
+++ resources/js/components/admin/TheSidebar.vue
@@ -0,0 +1,24 @@
+<script setup>
+import AdminNavItem from '../../components/AdminNavItem.vue';
+</script>
+
+<template>
+    <aside class="fixed z-10 top-12 left-0 w-64 h-[calc(100%-2.5rem)]" aria-label="Sidenav">
+        <div
+            class="overflow-y-auto py-5 px-3 h-full bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+            <ul class="space-y-2">
+                <AdminNavItem route-name="admin.dashboard">
+                    Dashboard
+                </AdminNavItem>
+
+                <AdminNavItem route-name="admin.assignments.index">
+                    Zadania
+                </AdminNavItem>
+                
+                <AdminNavItem route-name="admin.users.index">
+                    Používatelia
+                </AdminNavItem>
+            </ul>
+        </div>
+    </aside>
+</template>

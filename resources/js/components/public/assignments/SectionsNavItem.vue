c0351fa0b7633e93c9404029e1ed36ca699784e5
Erik Masny <erik.masny@gmail.com>
2022-12-19 22:38:37 +0100

    refactor: split assignment nav to sub components

---
+++ resources/js/components/public/assignments/SectionsNavItem.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineEmits([
+    'changeSection'
+])
+
+defineProps({
+    sectionId: String,
+    currentSection: String
+})
+</script>
+
+<template>
+    <li>
+        <button @click="$emit('changeSection', sectionId)"
+            :class="currentSection == sectionId ? 'text-primary' : 'text-sliver'"
+            class="font-semibold text-lg inline-flex items-center gap-x-2">
+
+            <slot></slot>
+            
+        </button>
+    </li>
+</template>

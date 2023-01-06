5b5785cf778c846752e038857ce2690d29e2c8e4
Erik Masny <erik.masny@gmail.com>
2023-01-06 17:19:58 +0100

    feat: show errors for all the missing inputs

--- resources/js/components/AssignmentForm.vue
+++ resources/js/components/AssignmentForm.vue
@@ -26,14 +26,12 @@
     
             <div class="mb-5">
                 <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Deadline</label>
-                <input type="datetime-local" v-model="assignment.deadline" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Názov zadania">
+                <AppInput type="datetime-local" v-model="assignment.deadline" :errors="errors?.deadline" />
             </div>
     
             <div class="mb-5">
-                <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Krátky popis</label>
-                <textarea 
-                    v-model="assignment.excerpt" 
-                    id="description" rows="3" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your description here"></textarea>
+                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Krátky popis</label>
+                <AppTextarea v-model="assignment.excerpt" placeholder="Krátky popis" :errors="errors?.excerpt" />
             </div>
     
             <div>
@@ -50,6 +48,7 @@ import { computed, inject } from 'vue';
 import AdminCard from './AdminCard.vue';
 import InputGroup from './admin/forms/InputGroup.vue';
 import AppInput from '@/components/admin/forms/AppInput.vue'
+import AppTextarea from './admin/forms/AppTextarea.vue';
 
 const assignment = inject('assignment')
 

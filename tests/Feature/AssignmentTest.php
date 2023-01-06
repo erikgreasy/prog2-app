f5ec2bbac6fefd009e069fd15f9c6e3a80b2cc2f
Erik Masny <erik.masny@gmail.com>
2023-01-06 18:02:10 +0100

    test: check assignment storing authorization

--- tests/Feature/AssignmentTest.php
+++ tests/Feature/AssignmentTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Enums\Role;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Assignment;
@@ -14,14 +15,76 @@ class AssignmentTest extends TestCase
 {
     use DatabaseMigrations;
 
-    public function test_store_endpoint_can_be_accessed_only_for_auth()
+    public function test_store_endpoint_cannot_be_accessed_by_non_logged_users()
     {
         $response = $this->postJson(route('assignments.store'));
-        $response->assertStatus(401);
 
-        Sanctum::actingAs(User::factory()->create());
+        $response->assertStatus(401);
+    }
+
+    public function test_store_endpoint_cannot_be_accessed_by_students()
+    {
+        Sanctum::actingAs(User::factory()->create(['role' => Role::STUDENT->value]));
+
         $response = $this->postJson(route('assignments.store'));
+
         $this->assertNotEquals(401, $response->getStatusCode());
+    }
+
+    public function test_store_endpoint_can_be_accessed_by_teacher()
+    {
+        Sanctum::actingAs(User::factory()->create(['role' => Role::TEACHER->value]));
+
+        $response = $this->postJson(route('assignments.store'));
+
+        $this->assertNotEquals(401, $response->getStatusCode());
+    }
+
+    public function test_store_endpoint_can_be_accessed_by_admin()
+    {
+        Sanctum::actingAs(User::factory()->create(['role' => Role::ADMIN->value]));
+
+        $response = $this->postJson(route('assignments.store'));
+
+        $this->assertNotEquals(401, $response->getStatusCode());
+    }
+
+    public function test_assignment_can_be_created()
+    {
+        Sanctum::actingAs(User::factory()->create(['role' => Role::ADMIN->value]));
+
+        $assignmentData = [
+            'title' => 'Assignment title',
+            'slug' => 'assignment-title',
+            'deadline' => now()->nextWeekday()->toDateTimeLocalString(),
+            'excerpt' => 'Lorem ipsum dolor sit'
+        ];
+
+        $this->postJson(route('assignments.store'), $assignmentData)->assertCreated();
+
+        $this->assertDatabaseHas('assignments', $assignmentData);
+    }
+
+    public function test_new_assignment_cannot_have_existing_slug()
+    {
+        Sanctum::actingAs(User::factory()->create(['role' => Role::ADMIN->value]));
+        
+        Assignment::factory()->create(['slug' => 'some-slug']);
+        
+        $this->postJson(route('assignments.store'), [
+            'title' => 'Assignment title',
+            'slug' => 'some-slug',
+            'deadline' => now()->nextWeekday()->toDateTimeLocalString(),
+            'excerpt' => 'Lorem ipsum dolor sit'
+        ])
+            ->assertStatus(422);
+
+        $this->assertDatabaseMissing('assignments', [
+            'title' => 'Assignment title',
+            'slug' => 'some-slug',
+            'deadline' => now()->nextWeekday()->toDateTimeLocalString(),
+            'excerpt' => 'Lorem ipsum dolor sit'
+        ]);
     }
 
     public function test_published_assignments_can_be_fetched_by_everyone()

1fa337179073112778b506ee8a0281406043ced1
Erik Masny <erik.masny@gmail.com>
2023-03-12 15:13:38 +0100

    feat: create component for code hightlight with prism

---
+++ resources/js/components/PrismCode.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Prism from "prismjs";
+import 'prismjs/themes/prism.min.css'
+import { onMounted } from "vue"
+
+defineProps({
+    lang: String,
+})
+
+onMounted(() => {
+    window.Prism = window.Prism || {};
+    window.Prism.manual = true;
+    Prism.highlightAll()
+})
+</script>
+
+<template>
+    <code :class="`language-${lang}`">
+        <slot></slot>
+    </code>
+</template>

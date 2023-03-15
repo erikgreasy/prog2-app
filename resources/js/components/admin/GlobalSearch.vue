c2b7f3cdb119361a7601144cb320d3655072790f
Erik Masny <erik.masny@gmail.com>
2023-03-15 21:42:13 +0100

    feat: make global search easier to use

--- resources/js/components/admin/GlobalSearch.vue
+++ resources/js/components/admin/GlobalSearch.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed, watch } from 'vue';
 import debounce from 'debounce'
 import AppInput from './forms/AppInput.vue';
 
@@ -9,7 +9,24 @@ const results = ref([])
 
 const loading = ref(false)
 
-const runSearch = debounce(async () => {
+const resultItems = computed(() => {
+    return results.value.map(group => group.results).flat()
+})
+
+const resolveRoute = (type, id) => {
+    if (type === 'assignment') {
+        return {name: 'admin.assignments.edit', params: {id: id}}
+    } else if (type === 'student') {
+        return {name: 'admin.students.show', params: {id: id}}
+    }
+}
+
+const clearSearch = () => {
+    results.value = []
+    searchKey.value = ''
+}
+
+watch(searchKey, debounce(async (e) => {
     results.value = []
     loading.value = true
 
@@ -27,30 +44,23 @@ const runSearch = debounce(async () => {
     } finally {
         loading.value = false
     }
-}, 500)
-
-const resolveRoute = (type, id) => {
-    if (type === 'assignment') {
-        return {name: 'admin.assignments.edit', params: {id: id}}
-    } else if (type === 'student') {
-        return {name: 'admin.students.show', params: {id: id}}
-    }
-}
-
-const clearSearch = () => {
-    results.value = []
-    searchKey.value = ''
-}
+}, 500))
 
 </script>
 
 <template>
     <div class="relative">
-        <AppInput v-model="searchKey" @keypress="runSearch" @change="runSearch" type="text" placeholder="Hľadať..." class="w-[400px] outline-none focus:ring-0 focus:shadown-none" />
+        <div class="relative">
+            <AppInput v-model="searchKey" type="text" placeholder="Hľadať..." class="w-[400px] outline-none focus:ring-0 focus:shadown-none" />
+            <span v-if="searchKey" @click="clearSearch" class="cursor-pointer absolute top-1/2 -translate-y-1/2 right-2.5 z-10">&times;</span>
+        </div>
 
-        <ul v-if="loading || results.length" class="absolute w-full bg-white p-5 shadow-xl border">
+        <ul v-if="loading || searchKey !== ''" class="absolute w-full bg-white p-5 shadow-xl border">
             <li v-if="loading">
                 Loading...
+            </li>
+            <li v-else-if="!resultItems.length">
+                Žiadne výsledky
             </li>
             <li v-else v-for="(group, index) in results" :key="index">
                 <div v-if="group.results.length">

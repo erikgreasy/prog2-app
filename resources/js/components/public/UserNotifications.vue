32bfcdc08e8bd3b87cc54eaa431fb7ce537d158d
Erik Masny <erik.masny@gmail.com>
2023-03-19 17:48:52 +0100

    feat: add transition to user notification

--- resources/js/components/public/UserNotifications.vue
+++ resources/js/components/public/UserNotifications.vue
@@ -11,28 +11,39 @@ onMounted(() => {
 
 <template>
     <div>
-        <div v-for="notification in notificationsStore.notifications" :key="notification.id" class="fixed bottom-10 w-full">
-            <div class="container">
-                <div class="bg-red-200 relative p-5">
-                    <span v-if="notification.type == 'App\\Notifications\\SubmissionProcessed'">
-                        Vaše odovzdanie z {{ notification.data.submission.created_at }} bolo vyhodnotené.
-                    </span>
-
-                        <RouterLink
-                            @click="notificationsStore.markAsRead(notification.id)"
-                            :to="{
-                                name: 'assignments.submissions.show', 
-                                params: {
-                                    slug: notification.data.submission.assignment?.slug,
-                                    index: notification.data.submission.try
-                                }
-                            }"
-                        >Zobrazit</RouterLink>
-                    <!-- {{ notification.data.message }} -->
-
-                    <button @click="notificationsStore.markAsRead(notification.id)">Close</button>
+        <Transition
+            enter-active-class="duration-300 transition"
+            enter-from-class="opacity-0"
+            enter-to-class="opacity-100"
+            leave-active-class="duration-200"
+            leave-from-class="opacity-100"
+            leave-to-class="opacity-0"
+        >
+            <div v-if="notificationsStore.notifications.length">
+                <div v-for="notification in notificationsStore.notifications" :key="notification.id" class="fixed bottom-10 w-full">
+                    <div class="container">
+                        <div class="bg-red-200 relative p-5">
+                            <span v-if="notification.type == 'App\\Notifications\\SubmissionProcessed'">
+                                Vaše odovzdanie z {{ notification.data.submission.created_at }} bolo vyhodnotené.
+                            </span>
+    
+                                <RouterLink
+                                    @click="notificationsStore.markAsRead(notification.id)"
+                                    :to="{
+                                        name: 'assignments.submissions.show', 
+                                        params: {
+                                            slug: notification.data.submission.assignment?.slug,
+                                            index: notification.data.submission.try
+                                        }
+                                    }"
+                                >Zobrazit</RouterLink>
+                            <!-- {{ notification.data.message }} -->
+    
+                            <button @click="notificationsStore.markAsRead(notification.id)">Close</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Transition>
     </div>
 </template>

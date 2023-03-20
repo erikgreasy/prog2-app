5c814e757232fb7450d08b92bdd6ab6bbadd4005
Erik Masny <erik.masny@gmail.com>
2023-03-20 14:56:33 +0100

    fix: typo causing not applying input error classes

--- resources/js/components/admin/forms/AppInput.vue
+++ resources/js/components/admin/forms/AppInput.vue
@@ -27,7 +27,7 @@ defineEmits(['update:modelValue'])
         @input="$emit('update:modelValue', $event.target.value)"
         :placeholder="placeholder"
         :class="[{
-            'bg-red-50 border border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500': errors?.lengthm,
+            'bg-red-50 border border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500': errors?.length,
             'bg-gray-300' : disabled
         }, additionalClasses]"
         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" 

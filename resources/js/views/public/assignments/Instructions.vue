68c1efd501178f20159a9ab6b1d0b81881fa9853
Erik Masny <erik.masny@gmail.com>
2023-03-25 20:58:21 +0100

    feat: show instructions with content parser

--- resources/js/views/public/assignments/Instructions.vue
+++ resources/js/views/public/assignments/Instructions.vue
@@ -1,11 +1,12 @@
 <script setup>
+import ContentParser from '@/components/public/assignments/ContentParser.vue';
 import { inject } from 'vue'
 
 const assignment = inject('assignment')
 </script>
 
 <template>
-    <section class="flex justify-center">
-        {{ assignment.submission_instructions }}
+    <section>
+        <ContentParser :content="assignment.submission_instructions" />
     </section>
 </template>

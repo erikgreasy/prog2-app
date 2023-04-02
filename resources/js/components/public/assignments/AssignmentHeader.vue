04ee72f9d7a8de8615de9ebe4da456dbd3301ef8
Erik Masny <erik.masny@gmail.com>
2023-04-02 20:56:33 +0200

    fix: remove unwanted margin from desktop

--- resources/js/components/public/assignments/AssignmentHeader.vue
+++ resources/js/components/public/assignments/AssignmentHeader.vue
@@ -9,7 +9,7 @@ defineProps({
         <h1 class="text-center font-extrabold text-4xl mb-5 mt-10 lg:mt-20">{{ assignment.title }}</h1>
 
         <div class="flex-col items-center lg:flex-row flex justify-center gap-x-4 lg:gap-x-8">
-            <div class="mb-4"><span class="font-semibold">Deadline:</span> <span class="text-primary font-semibold">{{ assignment.deadline?.readable }}</span></div>
+            <div class="mb-4 lg:mb-0"><span class="font-semibold">Deadline:</span> <span class="text-primary font-semibold">{{ assignment.deadline?.readable }}</span></div>
             <div class="text-xs lg:text-base"><span class="font-semibold">Max. počet bodov:</span> <span class="text-primary font-semibold">{{ assignment.points?.readable }}</span></div>
             <div class="text-xs lg:text-base"><span class="font-semibold">Zverejnené:</span> <span class="text-sliver">{{ assignment.published_at?.readable }}</span></div>
         </div>

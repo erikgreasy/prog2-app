03b458d8fca708467203edc9f1628395a22d8b26
Erik Masny <erik.masny@gmail.com>
2023-04-05 12:14:02 +0200

    fix: clear user notifications on logout

--- resources/js/composables/auth.js
+++ resources/js/composables/auth.js
@@ -1,15 +1,18 @@
 import { useRouter } from "vue-router"
 import { useAuthStore } from "../stores/auth"
+import { useUserNotificationsStore } from "@/stores/userNotifications"
 
 export function useAuth() {
     const router = useRouter()
     const authStore = useAuthStore()
+    const userNotificationsStore = useUserNotificationsStore()
 
     const logout = async() => {
         try {
             const res = await axios.post('/api/logout')
 
             authStore.logout()
+            userNotificationsStore.notifications = [] // clear user notifications from store, otherwise they would keep showing until refresh
             
             router.push({name: 'home'})
         } catch(err) {

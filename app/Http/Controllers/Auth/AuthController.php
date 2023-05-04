31555453a76a4c8101be1eecd80a797f15119175
Erik Masny <erik.masny@gmail.com>
2023-05-04 09:54:41 +0200

    wip: debug oauth err

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -20,22 +20,24 @@ class AuthController extends Controller
     {
         $googleUser = Socialite::driver('google')->user();
 
-        $user = User::updateOrCreate([
-            'email' => $googleUser->getEmail()
-        ], [
-            'username' => str_replace("@{$googleUser['user']['hd']}", '', $googleUser->getEmail()),
-            'first_name' => $googleUser['user']['given_name'],
-            'last_name' => $googleUser['user']['family_name'],
-            'full_name' => $googleUser->getName(),
-            'password' => Hash::make('examplePassword123')
-        ]);
+        dd($googleUser);
 
-        Auth::login($user);
-
-        if($user->role === Role::ADMIN->value || $user->role === Role::TEACHER->value) {
-            return redirect('/admin');
-        }
-
-        return redirect('/');
+//        $user = User::updateOrCreate([
+//            'email' => $googleUser->getEmail()
+//        ], [
+//            'username' => str_replace("@{$googleUser['user']['hd']}", '', $googleUser->getEmail()),
+//            'first_name' => $googleUser['user']['given_name'],
+//            'last_name' => $googleUser['user']['family_name'],
+//            'full_name' => $googleUser->getName(),
+//            'password' => Hash::make('examplePassword123')
+//        ]);
+//
+//        Auth::login($user);
+//
+//        if($user->role === Role::ADMIN->value || $user->role === Role::TEACHER->value) {
+//            return redirect('/admin');
+//        }
+//
+//        return redirect('/');
     }
 }

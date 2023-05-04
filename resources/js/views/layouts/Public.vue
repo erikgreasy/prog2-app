cae61f1346961baeaf5b6d2e6550250c2617ba57
Erik Masny <erik.masny@gmail.com>
2023-05-04 18:36:55 +0200

    fix: show name of logged user on public navbar

--- resources/js/views/layouts/Public.vue
+++ resources/js/views/layouts/Public.vue
@@ -72,7 +72,7 @@ const togglDarkMode = () => {
 
                     <NavbarItem v-else class="relative" ref="myprofileDropdown">
                         <span @click="myprofileDropdownOpen = !myprofileDropdownOpen" class="flex items-center text-primary gap-x-2 h-full">
-                            {{ authStore.user.name }}
+                            {{ authStore.user.full_name }}
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
                             </svg>

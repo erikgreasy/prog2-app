7936401126092e1d3c250b974c679ba3d651db91
Erik Masny <erik.masny@gmail.com>
2023-05-18 10:23:36 +0200

    fix: change wrong imgIndex function call to use prop

--- resources/js/components/public/assignments/ContentParserImage.vue
+++ resources/js/components/public/assignments/ContentParserImage.vue
@@ -9,6 +9,6 @@ defineProps({
     <figure class="mb-10">
         <img :src="block.data.file.url" class="mx-auto" :class="{'w-full': block.data.stretched}">
         <figcaption class="mt-3 text-center">
-            <strong>Obrázok {{ imgIndex(block.id) }}<span v-if="block.data.caption">:</span></strong> {{ block.data.caption }}</figcaption>
+            <strong>Obrázok {{ imgIndex }}<span v-if="block.data.caption">:</span></strong> {{ block.data.caption }}</figcaption>
     </figure>
 </template>
